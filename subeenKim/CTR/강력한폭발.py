74d6844a6aea308f6ada4534afb190200872b5b1
Kim-su-been <82082161+Kim-su-been@users.noreply.github.com>
2026-02-19 14:09:47 +0900

    🔀merge: [260218] 김수빈 문제풀이 (#122)
    * [BOJ] 1064 평행사변형
    
    * [BOJ] 1874 스택 수열
    
    * [BOJ] 27323 타슈
    
    * [BOJ] 1063 킹
    
    * [BOJ] 1244 스위치 켜고 끄기
    
    * [SWEA] gravity
    
    * [BOJ] 2644 촌수 계산
    
    * [SWEA] 1979. 어디에 단어가 들어갈 수 있을까
    
    * [CTR] 금 채굴하기
    
    * [SWEA] 회문2
    
    * [BOJ] 1406 에디터
    
    * [CTR] 네 방향 탈출 가능 여부 판별하기
    
    * [SWEA] Magnetic
    
    * [CTR] K번 최댓값으로 이동하기
    
    * [SWEA] 계산기2
    
    * [SWEA] 노드의거리
    
    * [CTR] K개 중에 1개를 N번 뽑기
    
    * [CTR] 마을 구분하기
    
    * [CTR] 안전지대
    
    * [CTR] 강력한 폭발
    
    ---------
    
    Co-authored-by: hyunjeekang <hihihyunjj@gmail.com>

---
+++ subeenKim/CTR/강력한폭발.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# 3종류의 폭탄이 터졌을 때 각각 초토화되는 지역
+bomb = [[(-2, 0), (-1, 0), (0, 0), (1, 0), (2, 0)], 
+    [(-1, 0), (1, 0), (0, 0), (0, -1), (0, 1)], 
+    [(-1, -1), (-1, 1), (0, 0), (1, -1), (1, 1)]]
+
+# 각 폭탄별로 폭탄 종류 - 중복 가능, 순서 있음 (0 -> 1번 폭탄, 1 -> 2번 폭탄, 2 -> 3번 폭탄)
+def bomb_kind(temp_list):
+    if len(temp_list) == bomb_cnt:
+        cases.append(temp_list[:])
+        return
+    for i in range(3):
+        temp_list.append(i)
+        bomb_kind(temp_list)
+        temp_list.pop()
+
+# 폭탄이 놓이는 위치 모으기
+bomb_loc = deque([])
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == 1:
+            bomb_loc.append((i, j))
+bomb_cnt = len(bomb_loc)
+
+cases = []
+bomb_kind([])
+max_cnt = 0
+for case in cases:
+    visited = [[False]*n for _ in range(n)]
+    cnt = 0
+    for k in range(bomb_cnt):
+        x, y = bomb_loc[k]
+        for dx, dy in bomb[case[k]]:
+            nx, ny = x+dx, y+dy
+            if 0 <= nx < n and 0 <= ny < n:
+                if not visited[nx][ny] :
+                    cnt += 1
+                    visited[nx][ny] = True
+    max_cnt = cnt if cnt > max_cnt else max_cnt
+print(max_cnt)
