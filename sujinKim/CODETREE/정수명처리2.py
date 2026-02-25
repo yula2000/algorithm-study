2e3a9d8686cf826fc7194b2b77b21186858d8f8a
edusoo83705 <soo83705@gmail.com>
2026-02-25 23:30:33 +0900

    feat:[CTR]원형순열에서의 인원제거

--- sujinKim/CODETREE/정수명처리2.py
+++ sujinKim/CODETREE/정수명처리2.py
@@ -1,3 +1,6 @@
+import sys
+sys.stdin = open('input.txt')
+
 N = int(input())
 command = []
 value = []
@@ -7,7 +10,7 @@ for _ in range(N):
     command.append(line[0])
     if line[0] == "push":
         value.append(int(line[1]))
-    elif line[0] == "pop": #명령어가 pop이라면 
+    elif line[0] == "pop": #명령어가 pop이라면
         p = value.pop(0)
         print(p)
     elif line[0] == "size":
@@ -15,13 +18,14 @@ for _ in range(N):
     elif line[0] == "empty":
         if len(value) == 0:
             print(1)
-        else: 
+        else:
             print(0)
     elif line[0] == "front":
         for i in range(len(value)):
             if value[i] == int(value[i]):
                 print(value[i])
-    else:  #명령어가 push가 아니라면 
+                break
+    else:  #명령어가 push가 아니라면
         value.append(0)  #A에 0번째를 추가한다. (즉, 다른 명령어)
 
 
