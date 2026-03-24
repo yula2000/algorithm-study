d6b7ea982e9b5b4732c1e581e63aeff0330897ae
흰머리 오목눈이 <soo83705@gmail.com>
2026-03-24 09:08:42 +0900

    🔀merge: [260324] 김수진 문제풀이 (#289)
    * feat:[BOJ] 1012 유기농 배추 re
    
    * feat:[BOJ] 2206 벽 부수고 이동하기
    
    * feat:[BOJ] 2504 괄호의 값
    
    * feat:[BOJ] 15650 N과 M(2)
    
    * feat:[BOJ]2504 괄호의 값 RE
    
    * feat:[BOJ] 15651 N과 M (3)
    
    * feat:[BOJ] 15654 N과 M(5)
    
    * feat:[BOJ] 15655 N과 M(6)
    
    * feat:[SWEA]1865 동철이의 일 분배
    
    * feat:[SWEA] 20551. 증가하는 사탕 수열
    
    * feat:[BOJ] 16933 벽 부수고 이동하기3
    
    * feat:[BOJ] 4153 직각삼각형
    
    * faet:[BOJ] 1260 DFS와 BFS
    
    * feat:[BOJ]별찍기
    
    * feat:[BOJ]2445 등 별찍기
    
    * feat:[BOJ]2446 별찍기 9
    
    * feat:[BOJ] 10992 별찍기 -17
    
    * feat:[BOJ]별찍기-17
    
    * feat:[BOJ]2448 도전문제
    
    * feat:[BOJ]10989 수 정렬하기 3
    
    ---------
    
    Co-authored-by: ParkKyuyeon <rose3518@naver.com>

---
+++ sujinKim/BOJ/10989.py
@@ -0,0 +1,37 @@
+import sys
+sys.stdin = open("input.txt")
+
+
+N = int(input())
+arr = list(int(input()) for _ in range(N))
+
+cnt = [0]*10001 # 1 ~ 10000 [0, 0, 0, 0, ....]
+
+for j in arr:
+    cnt[j] += 1
+
+# for i in range(max(arr)+1):  # arr의 가장 큰 수 (어차피 7까지만 있으니까)까지 하나씩 부르겠다
+#     for p in range(cnt[i]): # 근데 cnt의 i인덱스에 있는 수만큼 꺼내겠다
+#         print("\n".join(map(str,[i for _ in range(p)]))) #TypeError: can only join an iterable
+
+# "THIS IS PYTHON"
+# # ========== 1 ============
+# print("\n".join(str(i) for i in range(10001) for _ in range(cnt[i])))
+
+# # ========== 2 ============
+# result = []
+# for i in range(10001):
+#     if cnt[i] > 0:
+#         result.extend([str(i)]*cnt[i])
+#
+# # ['1', '1', ..]
+# print("\n".join(result))
+
+# " EASY MODE~"
+# # ========== 3 ============
+for i in range(10001):
+    for j in range(cnt[i]): # 알아서 j 횟수만큼 돌아감
+        print(i)
+
+# TypeError: sequence item 0: expected str instance, int found
+#iterable -> list 자료구조 but, 정수, 실수, 문자열, 참거짓 -> 자료형!
